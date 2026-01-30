ddde840b0c1e53d8dc4aa75da606a1a21dcf699e
lucianofmacedo2-ctrl <luciano.fmacedo2@gmail.com>
2026-01-30 09:50:21 -0300

    Update jogos.py

--- views/jogos.py
+++ views/jogos.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 
 # Links dos arquivos
 URL_AGENDA = "https://raw.githubusercontent.com/lucianofmacedo2-ctrl/BancaMasterLuciano/main/Lista_Jogos.csv"
-ARQUIVO_HISTORICO = 'dados_25_26.parquet'
+ARQUIVO_HISTORICO = 'dados_25_26.csv'
 
 def carregar_historico():
     try:
